e5b19c77a0776c876424c0d045dbdb60b87b731e
Maxim_belish <maxamax997@gmail.com>
2024-09-28 16:52:05 +0300

    start project PIZZA

---
+++ components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import React from 'react'
+import { cn } from '@/lib/utils'
+import { FilterCheckbox, FilterCheckboxProps } from './filter-сheckbox'
+import { Input } from '../ui'
+
+type Item = FilterCheckboxProps
+
+interface Props {
+	title: string
+	items: Item[]
+	defaultItems: Item[]
+	limit?: number
+	searchInputPlaceholder?: string
+	onChange?: (value: string[]) => void
+	defaultValue?: string[]
+	className?: string
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({ title, items, defaultItems, limit = 5, searchInputPlaceholder = 'Поиск...', className, onChange, defaultValue }) => {
+	const [showAll, setShowAll] = React.useState(false)
+	const [searchValue, setSearchValue] = React.useState('')
+
+	const list = showAll ? items.filter(item => item.text.toLowerCase().includes(searchValue.toLowerCase())) : defaultItems.slice(0, limit)
+
+	const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setSearchValue(e.target.value)
+	}
+	return (
+		<div className={cn(className)}>
+			<p className='font-bold mb-3'>{title}</p>
+			{showAll && (
+				<div className='mb-5'>
+					<Input onChange={onChangeSearchInput} placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
+				</div>
+			)}
+			<div className='flex flex-col gap-4 max-h-96 overflow-auto scrollbar'>
+				{list.map((item, index) => (
+					<FilterCheckbox key={index} text={item.text} value={item.value} endAdornment={item.endAdornment} checked={false} onCheckedChange={e => console.log(e)} />
+				))}
+			</div>
+			{items.length > limit && (
+				<div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+					<button onClick={() => setShowAll(!showAll)} className='text-primary mt-3'>
+						{showAll ? 'Скрыть' : '+ Показать все'}
+					</button>
+				</div>
+			)}
+		</div>
+	)
+}
